0cec4c76cc8f2ee1ae4663bedfd83dd28e6f78f2
Harper Sewalls <hsewalls@gmail.com>
2020-04-06 18:05:15 -0400

    sending functions_harper over

--- functions_harper.py
+++ functions_harper.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import healpy as hp
 
+#checks that the boolean masks partition the sky
 def test_masks(masks):
 	#making an array of the proper length to hold boolean values
 	vals = masks[0]
@@ -29,7 +30,7 @@ def map_and_mask(map,mask):
 
 	return(map)
 
-
+#function that combines all of the seperate maps into one
 def recombine_maps(maps, masks):
 	if(not test_masks(masks)):
 		return()
@@ -39,5 +40,32 @@ def recombine_maps(maps, masks):
 
 	new_maps = np.sum(new_maps, axis=0)
 	return(new_maps)
+
+#function that applies the weights to a given frequency range
+def weight_freqs(weights, freqs):
+
+	#checking that the weights and frequencies are the same length
+	if(not (len(weights)==len(freqs))):
+		print("ERROR:different number of weights and frequencies")
+		return()
+
+	#applying weights and returning the result
+	weighted = [weights[i]*freqs[i] for i in range(len(weights))]
+	return(np.sum(weighted))
+
+#function that takes edges of intervals and sets the masked
+#values to hp.UNSEEN so masking is easy
+def mask_outside_of_interval(interval, map):
+	print(map)
+	for i in range(len(map)):
+		if((not (interval[0] < map[i])) or (not (map[i] <= interval[1])) ):
+			map[i] = hp.UNSEEN
+
+	return(map)
+
+
+
+
+
 
 

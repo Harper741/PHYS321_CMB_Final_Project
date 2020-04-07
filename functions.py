1b6757674a9214e3ead3911fd1cce0c5f59c2c54
Harper Sewalls <hsewalls@gmail.com>
2020-04-07 18:40:15 -0400

    debugging map splitting function

--- functions.py
+++ functions.py
@@ -71,13 +71,13 @@ def weight_freqs(weights, freqs):
 
 #function that takes edges of intervals and sets the masked
 #values to hp.UNSEEN so masking is easy
-def mask_outside_of_interval(interval, map):
-	print(map)
-	for i in range(len(map)):
-		if((not (interval[0] < map[i])) or (not (map[i] <= interval[1])) ):
-			map[i] = hp.UNSEEN
+def mask_outside_of_interval(interval, the_map):
+    map2 = the_map
+	for i in range(len(the_map)):
+		if((not (interval[0] < the_map[i])) or (not (the_map[i] <= interval[1])) ):
+			map2[i] = hp.UNSEEN
 
-	return(map)
+	return(map2)
 
 #function to compute weights that minimize the variance of the sum of the channels
 
@@ -106,27 +106,28 @@ def compute_weights(freq_temps):
 
     return(weights)
 
-    def splitting_step1(regions, data_map): #FOR JUNK MAP
+def splitting_step1(regions, data_map): #FOR JUNK MAP
     #function that takes in the splitting regions and data maps and returns an array of regions cut out from the initial map as well as the boolean map
     #assumes regions is a list of intervals that partition the real line ex: [[-np.inf,-10],[-10,10],[10,np.inf]]
-       
+    
     cut_map = [] #cut out pieces of original data map
     bool_map = [] #entire map as bool values indicating whether a value is contained on a piece of a map or not
     size = [] #size of cut-out region
     
     for i in range(len(regions)):
         
-        temp_map = hf.mask_outside_of_interval(regions[i], data_map)
+        temp_map = mask_outside_of_interval(regions[i], data_map)
         bool_map.append(hp.pixelfunc.mask_bad(temp_map))
-        cut_map.append(data_map[bool_map[i] == True])
+        print(temp_map)
+        cut_map.append(data_map[bool_map[i] == False])
         size.append(len(cut_map))
     
-    if (np.sum(size) != len(data_map)):
-        print('Error: a pixel on a boundary was excluded or included in two or more regions, try changing the absolute tolerance')
+    ##if (np.sum(size) != len(data_map)):
+    ##    print('Error: a pixel on a boundary was excluded or included in two or more regions, try changing the absolute tolerance')
          
-    else:
-        return cut_map, bool_map
-    
+    ##else:
+    return([bool_map,cut_map])
+
 def splitting_step2(bool_maps, data_map): #for other maps, after junk map
     #function that takes in the splitting regions and data maps and returns an array of regions cut out from the initial map
        
